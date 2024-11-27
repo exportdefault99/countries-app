60580305f728c0035899ad054e033b8b5b2bf911
Leonid Filonenko <exportdefault99@gmail.com>
2024-11-27 05:23:46 +0300

    Updates

---
+++ assets/MainPage-BlZCIQ9W.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-BLvm8fUY.js";const t={},e=()=>s.jsx("div",{className:t.root,children:"MainPage"});export{e as default};
--- assets/MainPage-WX4v7rlm.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-BY0Pdp_s.js";const t={},e=()=>s.jsx("div",{className:t.root,children:"MainPage"});export{e as default};
