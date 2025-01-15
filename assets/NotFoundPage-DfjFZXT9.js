94913a671c659973c235500e257db89ef04ac043
Leonid Filonenko <exportdefault99@gmail.com>
2025-01-16 00:09:08 +0300

    Updates

--- assets/NotFoundPage-DfjFZXT9.js
+++
@@ -1,1 +0,0 @@
-import{k as o,E as s}from"./index-C_yR_D5F.js";const t="_root_1wupk_1",r={root:t},n=()=>o.jsx("section",{className:r.root,children:o.jsx(s,{children:o.jsx("p",{children:"Page not found"})})});export{n as default};
